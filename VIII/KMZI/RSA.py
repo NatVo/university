c8dfd1c3fe107f082d373d82dc52e70b02fd0d3f
root <root@localhost.localdomain>
2018-02-13 06:17:03 +0300

    rsa

--- VIII/KMZI/RSA.py
+++ VIII/KMZI/RSA.py
@@ -17,20 +17,42 @@ class RSA(Common):
         print(prime_p1, prime_p2)
 
         if (prime_p1 and prime_p2):
+            
+            _N = self.p1 * self.p2
+
             N = (self.p1 - 1) * (self.p2 - 1)
             print('\nN = {}'.format(N))
 
-            gcd_number = self.eiler_function(N)
-            print('Функция Эйлера от числа: {} = {}'.format(N, gcd_number))
-     
+            eiler_func = self.eiler_function(N)
+            print('\nФункция Эйлера от числа: {} = {}'.format(N, eiler_func))
 
+            mpr_list = self.find_mutually_prime(N)
+            print('\nВзаимно простые числа с числом {}: {}'.format(N, mpr_list))
+            
+            while (1):
+                try:
+                    i = int(input('\nВыберите взаимно простое число из списка: '))
+                    e = mpr_list[i]
+                    break
+                except:
+                    print('Некорректный ввод!')
+                    
+            print('\nЧисло e = {}'.format(e))
+            d = self.euklid_ext(N, e)
+            print('\nЧисло d = {}'.format(d))
+
+            print('\nЗакрытый ключ: ({}, {})'.format(_N, e))
+            print('\nОткрытый ключ: ({}, {})'.format(N, d))
+
+            self.dihotomy(25331478, 1472, 77)
+            #print(pow(25331478, 1472) % 77)
             #message = self.read_file()
             #print('\nИсходное сообщение: {}'.format(message))
     
        
 if __name__ == '__main__':
-    rsa = RSA(11,
-            7)
+    rsa = RSA(47,
+            71)
 
     rsa.encode()
     
--- VIII/KMZI/utils/common.py
+++ VIII/KMZI/utils/common.py
@@ -1,5 +1,8 @@
-from math import sqrt
 import numpy as np
+
+from math import sqrt
+from fractions import gcd
+
 
 class Common():
 
@@ -54,7 +57,75 @@ class Common():
         #print(fact, unique_fact)
 
         return (np.prod(fact) * np.prod(unique_fact))
-
-
+    
+    def gcd(self):
+        pass
+    
+    
+    def find_mutually_prime(self, number):
         
+        mpr_list = []
 
+        for i in range(1, number):
+            if (gcd(i, number) == 1):
+                mpr_list.append(i)
+        
+        return mpr_list
+    
+    def euklid_ext(self, a, b):
+
+        t1 = 0
+        t2 = 1
+        r = 1
+        
+        while (r != 0):
+            r = a % b
+            c = a // b
+
+            t3 = t1 - t2 * c
+            
+            #print(c, a, b, r, t1, t2, t3)
+
+            a = b
+            b = r
+
+            t1 = t2
+            t2 = t3
+
+        if (t1 < 0):
+            return (t2 + t1)
+        else:
+            return t1
+
+
+    def power_of_2(self, number):
+        counter = 1
+        pow_2 = 2
+
+        while (number >= pow_2):
+            #print(counter, two_power, '\n')
+            counter += 1
+            pow_2 *= 2
+        counter -= 1
+        return counter
+
+    def dihotomy(self, number, power, mod):
+
+        pow_2 = self.power_of_2(power)
+        pow_n = number
+        pow_list = [pow_n]
+
+        for i in range(1, pow_2 + 1):
+            pow_n = pow(pow_n, 2) % mod
+            pow_list.append(pow_n)
+
+        while (1):
+            left = power - pow(2, pow_2)
+            if (left < 1):
+                break
+            pow_2 = self.power_of_2(left)
+            pow_n = pow_n * pow_list[pow_2] % mod
+            #print('left: ', left, 'pow_2: ', pow_2, 'pow_n:', pow_n)
+            power = left
+        
+        print(pow_n)
